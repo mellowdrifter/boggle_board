1d930441fc310610a9e69bb3fc07447ca946b82d
Darren O'Connor <mellow.drifter@gmail.com>
2014-09-20 21:21:09 +0100

    Now draws a random board

--- boggle.py
+++ boggle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
- '''
+'''
  CUBES:
  ======
  New Version | Old Version
@@ -20,4 +20,32 @@
  AFFKPS      | DKNOTU
  HLNNRZ      | ADENVZ
  DEILRX      | BIFORX
- '''
+'''
+
+import random
+
+game_type_new=''
+letters=[]
+boggle_letters=[]
+
+if game_type_new:
+    squares=["AAEEGN","ELRTTY","AOOTTW","ABBJOO","EHRTVW","CIMOTV","DISTTY","EIOSST","DELRVY","ACHOPS","HIMNQU","EEINSU","EEGHNW","AFFKPS","HLNNRZ","DEILRX"]
+else:
+    squares=["AACIOT","AHMORS","EGKLUY","ABILTY","ACDEMP","EGINTV","GILRUW","ELPSTU","DENOSW","ACELRS","ABJMOQ","EEFHIY","EHINPS","DKNOTU","ADENVZ","BIFORX"]
+
+for i in range(0,16):
+    choice=random.randint(0,5)
+    letters.append(squares[i][choice])
+
+while len(letters)>0:
+    choice=random.randint(0,len(letters)-1)
+    boggle_letters.append(letters[choice])
+    letters.pop(choice)
+
+print "\nHere is your board:\n"
+for i in range(0,16):
+    print boggle_letters[i]+"  ",
+    if i==3 or i==7 or i==11:
+        print"\n"
+
+
