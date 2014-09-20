7c4fb6eec5b0a6326cceffc62abe95cd8b67953c
Darren O'Connor <mellow.drifter@gmail.com>
2014-09-20 19:21:24 +0100

    first commit

---
+++ boggle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+ '''
+ CUBES:
+ ======
+ New Version | Old Version
+ AAEEGN      | AACIOT
+ ELRTTY      | AHMORS
+ AOOTTW      | EGKLUY
+ ABBJOO      | ABILTY
+ EHRTVW      | ACDEMP
+ CIMOTV      | EGINTV
+ DISTTY      | GILRUW
+ EIOSST      | ELPSTU
+ DELRVY      | DENOSW
+ ACHOPS      | ACELRS
+ HIMNQU      | ABJMOQ
+ EEINSU      | EEFHIY
+ EEGHNW      | EHINPS
+ AFFKPS      | DKNOTU
+ HLNNRZ      | ADENVZ
+ DEILRX      | BIFORX
+ '''
